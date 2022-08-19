7e2f7c4e4175a0e635309cd53cac31a2597dd8ee
AudreyVK <audrey.klecha@gmx.de>
2022-08-19 10:11:59 +0200

    request.go

---
+++ request.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	c := http.Client{Timeout: time.Duration(3) * time.Second}
+
+	req, err := http.NewRequest("GET", "http://www.google.com/apple", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("User-Agent", "Go program")
+	resp, err := c.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+}
